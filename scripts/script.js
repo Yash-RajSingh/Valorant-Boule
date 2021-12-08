ca2a270af7fa8cd37e0d62181b638d1fb89ae7fa
Yash-RajSingh <yashraj45202@gmail.com>
2021-12-08 08:48:14 +0530

    Added Project

---
+++ scripts/script.js
@@ -0,0 +1,114 @@
+// 1-brim, 2-viper, 3-omen, 4-kj, 5-cypher, 6-sova, 7-sage, 8-pheonix, 9-jett, 
+// 10-reyna, 11-raze, 12-breach, 13-skye, 14-yoru, 15-astra, 16-kayo, 17-chamber
+const agents = [
+    {
+        name: "Brimstone",
+        picture: "./images/agents/01.png"
+    },
+    {
+        name: "Viper",
+        picture: "./images/agents/02.png"
+    },
+    {
+        name: "Omen",
+        picture: "./images/agents/03.png"
+    },
+    {
+        name: "KillJoy",
+        picture: "./images/agents/04.png"
+    },
+    {
+        name: "Cypher",
+        picture: "./images/agents/05.png"
+    },
+    {
+        name: "Sova",
+        picture: "./images/agents/06.png"
+    },
+    {
+        name: "Sage",
+        picture: "./images/agents/07.png"
+    },
+    {
+        name: "Phoenix",
+        picture: "./images/agents/08.png"
+    },
+    {
+        name: "Jett",
+        picture: "./images/agents/09.png"
+    },
+    {
+        name: "Reyna",
+        picture: "./images/agents/10.png"
+    },
+    {
+        name: "Raze",
+        picture: "./images/agents/11.png"
+    },
+    {
+        name: "Breach",
+        picture: "./images/agents/12.png"
+    },
+    {
+        name: "Skye",
+        picture: "./images/agents/13.png"
+    },
+    {
+        name: "Yoru",
+        picture: "./images/agents/14.png"
+    },
+    {
+        name: "Astra",
+        picture: "./images/agents/15.png"
+    },
+    {
+        name: "KAY/O",
+        picture: "./images/agents/16.png"
+    },
+    {
+        name: "Chamber",
+        picture: "./images/agents/17.png"
+    }
+]
+
+const selected = []
+const fun = () =>{
+    let x = Math.floor((Math.random() * 17) + 1);
+    return x;
+}
+
+for(i=0; i<=5 ; i++){
+    let x = fun();
+    if(selected.includes(x)==0)
+    {
+        selected.push(x);  
+    }
+    else{
+        selected.pop();
+    }
+    i=selected.length;
+    
+}
+
+const newlist = [];
+for(const a in selected){
+    let test = selected[a];
+    newlist[a] = agents[test];
+}
+console.log(newlist)
+const body = document.getElementById("container");
+const Amsg = document.getElementById("container0");
+var audio = new Audio('images/jackpot.mp3');
+const agentSelect = () =>{
+    audio.play();
+    Amsg.innerHTML =`<h2 id="a-msg">Your Agents Are</h2>`;
+    body.innerHTML = `
+    ${newlist.map(function(newlist){
+        return `
+        <div class="agentcard">
+            <img src="${newlist.picture}" height="300" width="190" class="a-img">
+            <h1>${newlist.name}</h1>
+            </div>`
+    }).join('')}`
+    window.location.href = "index.html#container";
+}
---
+++ scripts/tasks.js
@@ -0,0 +1,95 @@
+/* tasklist format {
+        name: ,
+        work:
+    }, */
+
+const tasklist = [
+    {
+        name: "The Wild West",
+        work: "All of the player must only use Sheriff for the whole round."
+    },
+    {
+        name: "Spartans",
+        work: "All of the players must rush to attack the enemy team but without buying armor."
+    },
+    {
+        name: "Spartans",
+        work: "All of the players must rush to attack the enemy team but without buying armor."
+    },
+    {
+        name: "Battle Cry",
+        work: `All of the players must continously yell "CHARGE!" throughout the round.`
+    },
+    {
+        name: "Fast as Fluff Boi",
+        work: "None of the players must not let go of the W key untill they are dead."
+    },
+    {
+        name: "Conquering Like A Kangaroo kangaroo",
+        work: "All of the players must continue to jump while moving and shooting."
+    },
+    {
+        name: "My World is Spinning",
+        work: "Everytime you encounter an enemy, you must 360 before you are allowed to shoot them."
+    },
+    {
+        name: "Bodyguards",
+        work: "The team must choose one of the players as their king and surround the king like bodyguards. The bodyguards must protect the king and must not let them die."
+    },
+    {
+        name: "Powerless",
+        work: "None of the players are allowed to purchase any of their abilities."
+    },
+    {
+        name: "Lurking in the Shadows",
+        work: "All of the players must camp at a spot of their choice and are only allowed to kill an enemy from that shadow spot. In case they are spotted by an enemy at a different location, they must rush to their shadow spot and then attack the enemy."
+    },
+    {
+        name: "The Expendables",
+        work: "All of the players must lower their health at the start of the round using a player's abilities and then march into battle."
+    },
+    {
+        name: "Knives out",
+        work: "All of the players are only allowed to kill an enemy using their knives."
+    },
+    {
+        name: "Simping Season",
+        work: "All of the males agents must play while constantly simping for the female agents throughout the round."
+    },
+    {
+        name: "Don't Breath!",
+        work: "All of the players must turn their speaker volumes to zero and play the round without any sound."
+    },
+    {
+        name: "Cloud Campers",
+        work: "Using Brimstone/Omen's smokes, the team must set smoke clouds at the default entry spots of their location and aren't allowed to exit the smoke cloud untill after they've disappeared."
+    }
+]
+
+
+
+
+const count = tasklist.length;
+const body2 = document.getElementById("container2");
+var current =0;
+var audio = new Audio('images/process.mp3');
+const taskSelect = () =>{
+    audio.play();
+    var numb = Math.floor((Math.random() * count) + 1);
+    if(numb==current){
+        numb=numb+1;
+    }
+    else if(numb==current || numb==count){
+        numb=numb-1;
+    }
+    current = numb;
+        const test = tasklist[numb];
+            body2.innerHTML = `<div class="taskcard">
+            <h1 class="t-msg">"${test.name}"</h1>
+            <h1 class="ttask">${test.work}</h1>
+            </div>`
+            window.location.href = "index.html#container2";
+        return current;
+}
+
+
